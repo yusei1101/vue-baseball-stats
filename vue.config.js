26e3fa95369e7c426795960b1c08e37ba632b045
Terada Yusei <yuseiterada@yusei-MacBook-Air.local>
2020-12-17 17:22:41 +0900

    20201217

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            scss: {
-                prependData: '@import "./src/assets/sass/_mixin.scss";',
-            }
-        }
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "./src/assets/sass/_mixin.scss";'
+      }
     }
+  }
 }
